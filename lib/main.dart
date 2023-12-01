3fc08d68e62b281531123b11559552451819fd58
David Messina <dmessina816@gmail.com>
2023-12-01 21:59:47 +0000

    MK-10 - Shared preferences setup
    * Added shared_preferences and initializing a provider in main() with an instance; Updated flutter_riverpod to 2.4.9
    
    Merged in MK-10 (pull request #10)

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mk8_randomizer/providers/shared_preferences_provider.dart';
 import 'package:mk8_randomizer/screens/track_selection.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
 
+  final sharedPreferences = await SharedPreferences.getInstance();
+
   runApp(
-    const ProviderScope(
-      child: MainApp(),
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: const MainApp(),
     ),
   );
 }
